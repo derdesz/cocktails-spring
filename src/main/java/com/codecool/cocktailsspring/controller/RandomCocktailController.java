a5cab73562f0162266d39055ce2ec4067fd5a70b
derdesz <dora.mailbox@gmail.com>
2020-11-10 14:18:56 +0100

    add get request for random cocktail

---
+++ src/main/java/com/codecool/cocktailsspring/controller/RandomCocktailController.java
@@ -0,0 +1,19 @@
+package com.codecool.cocktailsspring.controller;
+
+import com.codecool.cocktailsspring.model.detailedcocktail.DetailedCocktail;
+import com.codecool.cocktailsspring.service.CocktailAPIService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RandomCocktailController {
+
+    @Autowired
+    CocktailAPIService cocktailAPIService;
+
+    @GetMapping("/")
+    public DetailedCocktail getRandomcocktail() {
+        return cocktailAPIService.getRandomcocktail();
+    }
+}
