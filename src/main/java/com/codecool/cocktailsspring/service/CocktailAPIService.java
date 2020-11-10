97b4c31e05f288f649ddeb93b1afcbda4af5e1e1
derdesz <dora.mailbox@gmail.com>
2020-11-10 14:17:14 +0100

    get responseEntity body

--- src/main/java/com/codecool/cocktailsspring/service/CocktailAPIService.java
+++ src/main/java/com/codecool/cocktailsspring/service/CocktailAPIService.java
@@ -2,6 +2,7 @@ package com.codecool.cocktailsspring.service;
 
 import com.codecool.cocktailsspring.model.byspirit.CocktailsBySpirit;
 import com.codecool.cocktailsspring.model.byspirit.Spirit;
+import com.codecool.cocktailsspring.model.detailedcocktail.DetailedCocktail;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
@@ -12,13 +13,35 @@ import org.springframework.web.client.RestTemplate;
 public class CocktailAPIService {
 
     @Value("${cocktailsBySpirit.url}")
-    private String cocktailsBySpiritURL ;
+    private String cocktailsBySpiritURL;
+
+    @Value("${baseURL.url}")
+    private String baseURL;
+
+    @Value("${cocktailById.url}")
+    private String cocktailById;
 
     public CocktailsBySpirit getCocktailsBySpirit(Spirit spirit) {
         RestTemplate template = new RestTemplate();
         ResponseEntity<CocktailsBySpirit> cocktailsResponseEntity =
                 template.exchange(cocktailsBySpiritURL + spirit, HttpMethod.GET, null, CocktailsBySpirit.class);
         return cocktailsResponseEntity.getBody();
+    }
+
+
+    public DetailedCocktail getRandomcocktail() {
+        RestTemplate template = new RestTemplate();
+        ResponseEntity<DetailedCocktail> detailedCocktailResponseEntity =
+                template.exchange(baseURL + "random.php", HttpMethod.GET, null, DetailedCocktail.class);
+        return detailedCocktailResponseEntity.getBody();
+    }
+
+
+    public DetailedCocktail getCocktailById(int id) {
+        RestTemplate template = new RestTemplate();
+        ResponseEntity<DetailedCocktail> detailedCocktailResponseEntity =
+                template.exchange(cocktailById + id, HttpMethod.GET, null, DetailedCocktail.class);
+        return detailedCocktailResponseEntity.getBody();
     }
 
 }
