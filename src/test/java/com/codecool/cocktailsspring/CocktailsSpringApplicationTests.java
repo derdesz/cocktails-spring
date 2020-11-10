9afbfdab17d9363165124e6639a408641d4b8d50
derdesz <dora.mailbox@gmail.com>
2020-11-10 10:26:53 +0100

    initial commit

---
+++ src/test/java/com/codecool/cocktailsspring/CocktailsSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecool.cocktailsspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CocktailsSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
