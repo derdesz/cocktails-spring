94938cbc80206fbee2632250d7afec8e9dfc8c28
g-georgia <gg.georgina20@gmail.com>
2020-11-10 15:34:41 +0100

    Add Controller

---
+++ src/main/java/com/codecool/cocktailsspring/controller/CocktailsByAlcoholicController.java
@@ -0,0 +1,20 @@
+package com.codecool.cocktailsspring.controller;
+
+import com.codecool.cocktailsspring.model.listofcocktails.Alcoholic;
+import com.codecool.cocktailsspring.model.listofcocktails.ListOfDrinksItem;
+import com.codecool.cocktailsspring.service.CocktailAPIService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CocktailsByAlcoholicController {
+    @Autowired
+    CocktailAPIService cocktailAPIService;
+
+    @GetMapping("/{alcoholic}")
+    public ListOfDrinksItem getCocktailsBySpirit(@PathVariable("alcoholic") Alcoholic alcoholic){
+        return cocktailAPIService.getCocktailsByAlcoholic(alcoholic);
+    }
+}
