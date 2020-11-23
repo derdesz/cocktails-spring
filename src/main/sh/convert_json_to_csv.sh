0a56650f1e37178cca1266de812b0c51ebc2e296
g-georgia <gg.georgina20@gmail.com>
2020-11-23 15:19:07 +0100

    Convert response into csv

---
+++ src/main/sh/convert_json_to_csv.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+jq -r '.drinks[] | [.idDrink, .strDrink, .strAlcoholic, .strInstructions, .strDrinkThumb, .strIngredient1, .strIngredient2, .strIngredient3, .strIngredient4, .strIngredient5, .strIngredient6, .strIngredient7, .strIngredient8, .strIngredient9, .strIngredient10, .strIngredient11, .strIngredient12, .strIngredient13, .strIngredient14, .strIngredient15, .strMeasure1, .strMeasure2, .strMeasure3, .strMeasure4, .strMeasure5, .strMeasure6, .strMeasure7, .strMeasure8, .strMeasure9, .strMeasure10, .strMeasure11, .strMeasure12, .strMeasure13, .strMeasure14, .strMeasure15] | @csv' cocktails/*.json > ./cocktails.csv
