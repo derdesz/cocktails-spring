fbe952b2e786066783462e506458094dac796c9c
g-georgia <gg.georgina20@gmail.com>
2020-11-24 15:29:43 +0100

    Query cocktails by name

--- src/main/java/com/codecool/cocktailsspring/repository/CocktailRepository.java
+++ src/main/java/com/codecool/cocktailsspring/repository/CocktailRepository.java
@@ -1,7 +1,14 @@
 package com.codecool.cocktailsspring.repository;
 
 import com.codecool.cocktailsspring.entity.Cocktail;
+import com.codecool.cocktailsspring.model.Drinks;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface CocktailRepository extends JpaRepository<Cocktail, Long> {
+    @Query("SELECT c FROM Cocktail c WHERE c.strDrink LIKE %:name%")
+    List<Cocktail> findCocktailsByStrDrink(@Param ("name") String name);
 }
