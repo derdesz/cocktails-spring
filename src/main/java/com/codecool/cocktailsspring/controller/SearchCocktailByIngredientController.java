2ae106e42a0f46e36451b891b4f86f7dfbdd330e
g-georgia <gg.georgina20@gmail.com>
2020-11-25 10:26:57 +0100

    Pass search by ingredients query result into controller

--- src/main/java/com/codecool/cocktailsspring/controller/SearchCocktailByIngredientController.java
+++ src/main/java/com/codecool/cocktailsspring/controller/SearchCocktailByIngredientController.java
@@ -1,7 +1,9 @@
 package com.codecool.cocktailsspring.controller;
 
+import com.codecool.cocktailsspring.entity.Cocktail;
 import com.codecool.cocktailsspring.model.detailedcocktail.DetailedCocktail;
 import com.codecool.cocktailsspring.model.listofcocktails.ListOfDrinksItems;
+import com.codecool.cocktailsspring.repository.CocktailRepository;
 import com.codecool.cocktailsspring.service.CocktailAPIService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -9,14 +11,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @CrossOrigin(origins = "http://localhost:3000")
 @RestController
 public class SearchCocktailByIngredientController {
     @Autowired
     CocktailAPIService cocktailAPIService;
 
+    @Autowired
+    CocktailRepository cocktailRepository;
+
     @GetMapping("/search/ingredient/{ingredient}")
-    public ListOfDrinksItems searchCocktailByIngredient(@PathVariable("ingredient") String ingredient) {
-        return cocktailAPIService.searchCocktailByIngredient(ingredient);
+    public List<Cocktail> findCocktailByStrIngredient(@PathVariable("ingredient") String ingredient) {
+        return cocktailRepository.findCocktailByStrIngredient(ingredient);
     }
 }
