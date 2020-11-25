b339dc213a1b63baa06630ac5cd02a3f85ab7cdc
g-georgia <gg.georgina20@gmail.com>
2020-11-25 10:26:12 +0100

    Query cocktail search by ingredients

--- src/main/java/com/codecool/cocktailsspring/repository/CocktailRepository.java
+++ src/main/java/com/codecool/cocktailsspring/repository/CocktailRepository.java
@@ -11,4 +11,7 @@ import java.util.List;
 public interface CocktailRepository extends JpaRepository<Cocktail, Long> {
     @Query("SELECT c FROM Cocktail c WHERE c.strDrink LIKE %:name%")
     List<Cocktail> findCocktailsByStrDrink(@Param ("name") String name);
+
+    @Query("SELECT c FROM Cocktail c WHERE c.allIngredients LIKE %:ingredient%")
+            List<Cocktail> findCocktailByStrIngredient(@Param ("ingredient") String ingredient);
 }
