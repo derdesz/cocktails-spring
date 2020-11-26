f4d98499df78344b7bd9bb1ac98a109bdc7caee4
derdesz <dora.mailbox@gmail.com>
2020-11-26 13:48:21 +0100

    add new cocktail to database

--- src/main/java/com/codecool/cocktailsspring/controller/SaveCocktailRecipe.java
+++ src/main/java/com/codecool/cocktailsspring/controller/SaveCocktailRecipe.java
@@ -1,16 +1,10 @@
 package com.codecool.cocktailsspring.controller;
 
-import com.codecool.cocktailsspring.entity.Cocktail;
 import com.codecool.cocktailsspring.model.NewCocktail;
-import com.codecool.cocktailsspring.model.detailedcocktail.DetailedCocktail;
 import com.codecool.cocktailsspring.repository.CocktailRepository;
-import com.codecool.cocktailsspring.service.CocktailAPIService;
 import com.codecool.cocktailsspring.service.CocktailDBService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.HashMap;
-
 
 @RestController
 public class SaveCocktailRecipe {
@@ -24,8 +18,6 @@ public class SaveCocktailRecipe {
     @CrossOrigin(origins = "http://localhost:3000")
     @PostMapping("/save-new-cocktail")
     public void createCocktail(@RequestBody NewCocktail newCocktail) {
-        System.out.println("save " + newCocktail);
         cocktailDBService.createCocktail(newCocktail);
-        cocktailRepository.insertCocktail(newCocktail.getStrDrink(), newCocktail.getStrAlcoholic(), newCocktail.getStrInstructions(), cocktailDBService.createStringsFromList(newCocktail.getAllIngredients()));
     }
 }
--- src/main/java/com/codecool/cocktailsspring/repository/CocktailRepository.java
+++ src/main/java/com/codecool/cocktailsspring/repository/CocktailRepository.java
@@ -1,9 +1,7 @@
 package com.codecool.cocktailsspring.repository;
 
 import com.codecool.cocktailsspring.entity.Cocktail;
-import com.codecool.cocktailsspring.model.Drinks;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
@@ -21,14 +19,5 @@ public interface CocktailRepository extends JpaRepository<Cocktail, Long> {
 
     @Query("SELECT c FROM Cocktail c WHERE c.strAlcoholic LIKE %:alcoholic%")
     List<Cocktail> findCocktailsByStAndStrAlcoholic(@Param ("alcoholic") String alcoholic);
-
-
-    @Modifying
-    @Query(
-            value =
-                    "insert into Cocktail (strDrink, strAlcoholic, strInstructions, strAllIngredients) values (:strDrink, :strAlcoholic, :strInstructions, :strAllIngredients)",
-            nativeQuery = true)
-    void insertCocktail(@Param("strDrink") String strDrink, @Param("strAlcoholic") String strAlcoholic,
-                    @Param("strInstructions") String strInstructions, @Param("strAllIngredients") String strAllIngredients);
 
 }
