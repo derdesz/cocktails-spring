bd0057f0a303d3af9dd1af9fd84467b910bc6c86
g-georgia <gg.georgina20@gmail.com>
2020-11-27 15:22:09 +0100

    Make NewCocktail an Entity

--- src/main/java/com/codecool/cocktailsspring/model/NewCocktail.java
+++ src/main/java/com/codecool/cocktailsspring/model/NewCocktail.java
@@ -4,13 +4,21 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
 import javax.persistence.Lob;
 import java.util.List;
 
+@Entity
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class NewCocktail {
+    @Id
+    private String idDrink;
+
+    @Column(unique = true)
     private String strDrink;
     private String strAlcoholic;
 
