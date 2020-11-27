65a072814380b6adc0eed3cf599d469158799662
g-georgia <gg.georgina20@gmail.com>
2020-11-27 15:21:16 +0100

    Create a mapper for NewCocktails

---
+++ src/main/java/com/codecool/cocktailsspring/model/NewCocktailMapper.java
@@ -0,0 +1,21 @@
+package com.codecool.cocktailsspring.model;
+
+import com.codecool.cocktailsspring.entity.Cocktail;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class NewCocktailMapper {
+    public static NewCocktail cocktailMapper(Cocktail cocktail){
+        return new NewCocktail(cocktail.getIdDrink(), cocktail.getStrDrink(), cocktail.getStrAlcoholic(), cocktail.getStrInstructions(), cocktail.createListOfIngredients());
+    }
+
+    public static List<NewCocktail> listNewCocktails(Drinks drinks){
+        List<NewCocktail> newCocktailList = new ArrayList<>();
+        for (Cocktail cocktail: drinks.getDrinks()){
+            newCocktailList.add(cocktailMapper(cocktail));
+        }
+        return newCocktailList;
+    }
+}
