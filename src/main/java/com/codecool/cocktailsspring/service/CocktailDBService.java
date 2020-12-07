fcff8b5ba5aeab16c7739bed290a3dafd944ef93
derdesz <dora.mailbox@gmail.com>
2020-12-07 16:30:36 +0100

    get cocktails from all api endpoints through the alphabet

--- src/main/java/com/codecool/cocktailsspring/service/CocktailDBService.java
+++ src/main/java/com/codecool/cocktailsspring/service/CocktailDBService.java
@@ -1,12 +1,10 @@
 package com.codecool.cocktailsspring.service;
 
-import com.codecool.cocktailsspring.entity.Cocktail;
 import com.codecool.cocktailsspring.model.Drinks;
 import com.codecool.cocktailsspring.model.NewCocktail;
 import com.codecool.cocktailsspring.model.NewCocktailMapper;
 import com.codecool.cocktailsspring.repository.CocktailRepository;
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpMethod;
@@ -14,14 +12,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 @Service
 public class CocktailDBService {
@@ -32,40 +26,28 @@ public class CocktailDBService {
     @Value("${cocktailsByLetter.url}")
     private String COCKTAILS_BY_LETTER;
 
-//    public void importCocktailData(){
-//        ObjectMapper objectMapper = new ObjectMapper()
-//                .configure(DeserializationFeature.USE_JAVA_ARRAY_FOR_JSON_ARRAY, true)
-//                .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-//        try (Stream<Path> paths = Files.walk(Paths.get("./cocktails"))) {
-//            paths
-//                    .filter(Files::isRegularFile)
-//                    .forEach( it -> {
-//                        try {
-//                            Drinks drinks = objectMapper.readValue(it.toFile(), Drinks.class);
-//                            if (drinks.getDrinks() != null) {
-//                                for (Cocktail cocktail : drinks.getDrinks()) {
-//                                    cocktailRepository.save(cocktail);
-//                                }
-//                            }
-//                        } catch (IOException e) {
-//                            e.printStackTrace();
-//                        }
-//                    });
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//    }
 
-    public void importJSONData(){
+    public void importJSONData() {
+        char currentChar = 'a';
+        List<List<NewCocktail>> newCocktails = new ArrayList<>();
         RestTemplate restTemplate = new RestTemplate();
-        ResponseEntity<Drinks> responseEntity = restTemplate.exchange(COCKTAILS_BY_LETTER.concat("a"), HttpMethod.GET, null, Drinks.class);
-        List<NewCocktail> newCocktails = NewCocktailMapper.listNewCocktails(responseEntity.getBody());
-        for (NewCocktail cocktail : newCocktails) {
+        for (char character = currentChar; character <= 'z'; character++) {
+            try {
+                ResponseEntity<Drinks> responseEntity = restTemplate.exchange(COCKTAILS_BY_LETTER.concat(String.valueOf(character)), HttpMethod.GET, null, Drinks.class);
+                newCocktails.add(NewCocktailMapper.listNewCocktails(responseEntity.getBody()));
+            } catch (NullPointerException e) {
+                continue;
+            }
+        }
+
+        List<NewCocktail> allNewCocktailsList = newCocktails.stream().flatMap(List::stream).collect(Collectors.toList());
+        for (NewCocktail cocktail : allNewCocktailsList) {
             cocktailRepository.save(cocktail);
         }
     }
 
-    public String createStringsFromList (List<String> list) {
+
+    public String createStringsFromList(List<String> list) {
         StringBuilder sb = new StringBuilder();
         for (String listItem : list) {
             sb.append(listItem);
@@ -74,7 +56,7 @@ public class CocktailDBService {
         return sb.toString().trim();
     }
 
-    public String capitalize (String string) {
+    public String capitalize(String string) {
         String firstLetter = String.valueOf(string.charAt(0)).toUpperCase();
         String restOfLetters = string.substring(1);
         System.out.println(firstLetter + restOfLetters);
@@ -92,12 +74,12 @@ public class CocktailDBService {
         cocktailRepository.save(cocktail);
     }
 
-    public List<NewCocktail> filterCocktailsByIngredient(String ingredientName){
+    public List<NewCocktail> filterCocktailsByIngredient(String ingredientName) {
         return cocktailRepository.findAll()
                 .stream()
                 .filter(newCocktail -> {
                     for (String ingredient : newCocktail.getAllIngredients()) {
-                        if (ingredient.contains(ingredientName)){
+                        if (ingredient.contains(ingredientName)) {
                             return true;
                         }
                     }
