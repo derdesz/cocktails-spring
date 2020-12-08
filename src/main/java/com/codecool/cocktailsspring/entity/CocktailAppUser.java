71d4b3eed79054db0699f81ba50a17522299dbc9
g-georgia <gg.georgina20@gmail.com>
2020-12-08 15:35:25 +0100

    Create cocktailAppUser class

--- src/main/java/com/codecool/cocktailsspring/entity/CocktailAppUser.java
+++ src/main/java/com/codecool/cocktailsspring/entity/CocktailAppUser.java
@@ -1,15 +1,17 @@
 package com.codecool.cocktailsspring.entity;
 
+import com.codecool.cocktailsspring.model.Role;
 import lombok.*;
 
-import javax.persistence.ElementCollection;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
 @Entity
+@Table(	name = "cocktail_app_user",
+        uniqueConstraints = {
+                @UniqueConstraint(columnNames = "email")
+        })
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -25,8 +27,27 @@ public class CocktailAppUser {
     @NonNull
     private String name;
 
+    @ManyToMany(fetch = FetchType.LAZY, cascade = {CascadeType.ALL})
+    @JoinTable(	name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles = new HashSet<>();
+
+    public CocktailAppUser(@NonNull String email, @NonNull String password, @NonNull String name) {
+        this.email = email;
+        this.password = password;
+        this.name = name;
+    }
+
     @ElementCollection
     private Set<Long> favouriteCocktailIds = new HashSet<>();
+
+    public CocktailAppUser(@NonNull String email, @NonNull String password, @NonNull String name, Set<Role> roles) {
+        this.email = email;
+        this.password = password;
+        this.name = name;
+        this.roles = roles;
+    }
 
     public void addFavouriteCocktail(Long cocktailId){
         favouriteCocktailIds.add(cocktailId);
