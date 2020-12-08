1bd576a5bf919e6c1e78299c6f7e398a9766471f
g-georgia <gg.georgina20@gmail.com>
2020-12-08 15:41:44 +0100

    Create UserDetailsService

---
+++ src/main/java/com/codecool/cocktailsspring/security/UserDetailsServiceImpl.java
@@ -0,0 +1,25 @@
+package com.codecool.cocktailsspring.security;
+
+import com.codecool.cocktailsspring.entity.CocktailAppUser;
+import com.codecool.cocktailsspring.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService{
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        CocktailAppUser user = userRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User Not Found with username: " + username));
+
+        return UserDetailsImpl.build(user);
+    }
+}
