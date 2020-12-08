b1f19bf9e37a488dc173acb265b6ce02328656d6
g-georgia <gg.georgina20@gmail.com>
2020-12-08 15:37:23 +0100

    Create Role with repository

---
+++ src/main/java/com/codecool/cocktailsspring/model/Role.java
@@ -0,0 +1,30 @@
+package com.codecool.cocktailsspring.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(length = 20)
+    private ERole name;
+
+    public Role(ERole name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/com/codecool/cocktailsspring/repository/RoleRepository.java
@@ -0,0 +1,13 @@
+package com.codecool.cocktailsspring.repository;
+
+import com.codecool.cocktailsspring.model.ERole;
+import com.codecool.cocktailsspring.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(ERole name);
+}
