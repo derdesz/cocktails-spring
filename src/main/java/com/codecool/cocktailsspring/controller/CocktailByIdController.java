89a8fe6f25466d2d1bf44836d0df84a373498ca6
g-georgia <gg.georgina20@gmail.com>
2021-02-23 16:54:24 +0100

    Remove unnecessary CocktailAPIService injections

--- src/main/java/com/codecool/cocktailsspring/controller/CocktailByIdController.java
+++ src/main/java/com/codecool/cocktailsspring/controller/CocktailByIdController.java
@@ -2,7 +2,6 @@ package com.codecool.cocktailsspring.controller;
 
 import com.codecool.cocktailsspring.model.NewCocktail;
 import com.codecool.cocktailsspring.repository.CocktailRepository;
-import com.codecool.cocktailsspring.service.CocktailAPIService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,9 +11,6 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin(origins = "http://localhost:3000")
 @RestController
 public class CocktailByIdController {
-
-    @Autowired
-    CocktailAPIService cocktailAPIService;
 
     @Autowired
     CocktailRepository cocktailRepository;
--- src/main/java/com/codecool/cocktailsspring/controller/CocktailsByAlcoholicController.java
+++ src/main/java/com/codecool/cocktailsspring/controller/CocktailsByAlcoholicController.java
@@ -2,7 +2,6 @@ package com.codecool.cocktailsspring.controller;
 
 import com.codecool.cocktailsspring.model.NewCocktail;
 import com.codecool.cocktailsspring.repository.CocktailRepository;
-import com.codecool.cocktailsspring.service.CocktailAPIService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,8 +13,6 @@ import java.util.List;
 @CrossOrigin(origins = "http://localhost:3000")
 @RestController
 public class CocktailsByAlcoholicController {
-    @Autowired
-    CocktailAPIService cocktailAPIService;
 
     @Autowired
     CocktailRepository cocktailRepository;
