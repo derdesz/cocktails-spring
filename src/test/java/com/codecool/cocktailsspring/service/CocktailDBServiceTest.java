4dbf9c4e9918668570c1299f6177215c37a819d5
derdesz <dora.mailbox@gmail.com>
2021-02-24 11:16:13 +0100

    add test for newExistingCocktailIsSavedToRepository & filterCocktailsContainingIngredient

--- src/test/java/com/codecool/cocktailsspring/service/CocktailDBServiceTest.java
+++ src/test/java/com/codecool/cocktailsspring/service/CocktailDBServiceTest.java
@@ -13,14 +13,17 @@ import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.client.RestTemplate;
 
 import java.nio.charset.Charset;
 import java.util.*;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@Transactional
 class CocktailDBServiceTest {
 
     @Autowired
@@ -62,9 +65,8 @@ class CocktailDBServiceTest {
     }
 
 
-
     @Test
-    void newCocktailIsSavedToRepository() {
+    void newUniqueCocktailIsSavedToRepository() {
         NewCocktail newCocktail = new NewCocktail();
         byte[] array = new byte[7]; // length is bounded by 7
         new Random().nextBytes(array);
@@ -78,7 +80,65 @@ class CocktailDBServiceTest {
         Assertions.assertEquals(randomName, newCocktail.getStrDrink());
     }
 
+
     @Test
-    void filterCocktailsByIngredient() {
+    void newExistingCocktailIsSavedToRepository() {
+        NewCocktail newCocktail = new NewCocktail();
+        byte[] array = new byte[7]; // length is bounded by 7
+        new Random().nextBytes(array);
+        String randomName = new String(array, Charset.forName("UTF-8"));
+        newCocktail.setIdDrink(UUID.randomUUID().toString());
+        newCocktail.setStrDrink(randomName);
+        newCocktail.setStrAlcoholic("alcoholic");
+        newCocktail.setStrInstructions("instruction");
+        newCocktail.setAllIngredients(new ArrayList<String>(Collections.singleton("ingr1, ingr2")));
+        cocktailRepository.save(newCocktail);
+
+        NewCocktail newCocktail1 = new NewCocktail();
+        newCocktail1.setIdDrink(UUID.randomUUID().toString());
+        newCocktail1.setStrDrink(randomName);
+        newCocktail1.setStrAlcoholic("alcoholic");
+        newCocktail1.setStrInstructions("instruction");
+        newCocktail1.setAllIngredients(new ArrayList<String>(Collections.singleton("ingr1, ingr2")));
+        Assertions.assertThrows(DataIntegrityViolationException.class, () -> cocktailRepository.save(newCocktail1));
+    }
+
+    @Test
+    void filterCocktailsContainingIngredient() {
+        NewCocktail newCocktail1 = new NewCocktail();
+        byte[] array = new byte[7]; // length is bounded by 7
+        new Random().nextBytes(array);
+        String randomName = new String(array, Charset.forName("UTF-8"));
+        newCocktail1.setIdDrink(UUID.randomUUID().toString());
+        newCocktail1.setStrDrink(randomName);
+        newCocktail1.setStrAlcoholic("alcoholic");
+        newCocktail1.setStrInstructions("instruction");
+        newCocktail1.setAllIngredients(new ArrayList<String>(Collections.singleton("ingr1, ingr2")));
+        cocktailRepository.save(newCocktail1);
+
+        NewCocktail newCocktail2 = new NewCocktail();
+        byte[] array2 = new byte[7]; // length is bounded by 7
+        new Random().nextBytes(array2);
+        String randomName2 = new String(array2, Charset.forName("UTF-8"));
+        newCocktail2.setIdDrink(UUID.randomUUID().toString());
+        newCocktail2.setStrDrink(randomName2);
+        newCocktail2.setStrAlcoholic("alcoholic");
+        newCocktail2.setStrInstructions("instruction");
+        newCocktail2.setAllIngredients(new ArrayList<String>(Collections.singleton("ingr1, ingr2")));
+        cocktailRepository.save(newCocktail2);
+
+        List<NewCocktail> cocktailsHavingSpecificIngredient = cocktailRepository.findAll()
+                .stream()
+                .filter(newCocktail -> {
+                    for (String ingredient : newCocktail.getAllIngredients()) {
+                        if (ingredient.contains("in")) {
+                            return true;
+                        }
+                    }
+                    return false;
+                })
+                .collect(Collectors.toList());
+
+        Assertions.assertTrue(cocktailsHavingSpecificIngredient.contains(newCocktail1)&&cocktailsHavingSpecificIngredient.contains(newCocktail2));
     }
 }
