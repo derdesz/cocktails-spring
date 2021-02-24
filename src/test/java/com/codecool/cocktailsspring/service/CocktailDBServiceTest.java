5456f01e650bc79a1e56f5bd3dcda07dcedc9272
derdesz <dora.mailbox@gmail.com>
2021-02-24 09:20:58 +0100

    add test for newCocktailIsSavedToRepository

--- src/test/java/com/codecool/cocktailsspring/service/CocktailDBServiceTest.java
+++ src/test/java/com/codecool/cocktailsspring/service/CocktailDBServiceTest.java
@@ -3,17 +3,28 @@ package com.codecool.cocktailsspring.service;
 import com.codecool.cocktailsspring.model.Drinks;
 import com.codecool.cocktailsspring.model.NewCocktail;
 import com.codecool.cocktailsspring.model.NewCocktailMapper;
+import com.codecool.cocktailsspring.repository.CocktailRepository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.nio.charset.Charset;
+import java.util.*;
 import java.util.concurrent.atomic.AtomicInteger;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest
 class CocktailDBServiceTest {
+
+    @Autowired
+    CocktailRepository cocktailRepository;
 
     @Test
     void importedJsonContainsAll415CocktailRecipes() {
@@ -40,7 +51,7 @@ class CocktailDBServiceTest {
 
 
     @Test
-    void capitalize() {
+    void capitalizeStringStartingWithLowercaseLetter() {
         String word = "hello";
         String firstLetter = String.valueOf(word.charAt(0)).toUpperCase();
         String restOfLetters = word.substring(1);
@@ -50,8 +61,21 @@ class CocktailDBServiceTest {
 
     }
 
+
+
     @Test
-    void createCocktail() {
+    void newCocktailIsSavedToRepository() {
+        NewCocktail newCocktail = new NewCocktail();
+        byte[] array = new byte[7]; // length is bounded by 7
+        new Random().nextBytes(array);
+        String randomName = new String(array, Charset.forName("UTF-8"));
+        newCocktail.setIdDrink(UUID.randomUUID().toString());
+        newCocktail.setStrDrink(randomName);
+        newCocktail.setStrAlcoholic("alcoholic");
+        newCocktail.setStrInstructions("instruction");
+        newCocktail.setAllIngredients(new ArrayList<String>(Collections.singleton("ingr1, ingr2")));
+        cocktailRepository.save(newCocktail);
+        Assertions.assertEquals(randomName, newCocktail.getStrDrink());
     }
 
     @Test
