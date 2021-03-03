e935b3297543c0f2f2f587495ceabd2cd1e4401d
g-georgia <gg.georgina20@gmail.com>
2021-03-03 15:59:52 +0100

    Invalidate session at logout

--- src/main/java/com/codecool/cocktailsspring/controller/AuthController.java
+++ src/main/java/com/codecool/cocktailsspring/controller/AuthController.java
@@ -24,8 +24,10 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 import java.util.*;
 import java.util.stream.Collectors;
@@ -119,13 +121,17 @@ public class AuthController {
                 roles));
     }
 
-    @GetMapping("/logout")
-    public String logOutUser(HttpServletRequest request, HttpServletResponse response) {
+    @GetMapping("/log-out")
+    public ResponseEntity<String> logOutUser(HttpServletRequest request, HttpServletResponse response) {
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        if (auth != null){
-            new SecurityContextLogoutHandler().logout(request, response, auth);
+        response.addCookie(new Cookie("auth", ""));
+        response.addCookie(new Cookie("email", ""));
+
+        HttpSession session = request.getSession(false);
+        if (session != null) {
+            session.invalidate();
         }
-        return "redirect:/login?logout";
+        return ResponseEntity.ok("OK");
     }
 
     @GetMapping("/login_error")
